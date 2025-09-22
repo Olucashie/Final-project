6dce07430d88c92c8a9340fe5080267586278d94
Oluwabori Testimony <olucashie@gmail.com>
2025-09-22 08:09:52 -0700

    Progress

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,4 +1,4 @@
-export const API_BASE =  'http://localhost:5000/api'
+export const API_BASE =  'https://final-project-1-zwia.onrender.com/api'
 
 export function authHeaders(token) {
   return token ? { Authorization: `Bearer ${token}` } : {}
