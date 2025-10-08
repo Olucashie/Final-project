4ad051e8cbec96d01f36c17d43f12f37d538b1b0
Oluwabori Testimony <olucashie@gmail.com>
2025-10-08 10:23:04 -0700

    updated

--- api/server.js
+++ api/server.js
@@ -1,18 +1,10 @@
 const dotenv = require('dotenv');
+dotenv.config();
 const connectDB = require('./config/db');
 const app = require('./app');
-const cors = require('cors');
-
-app.use(cors({
-  origin: [
-    'https://unihost-project.vercel.app'
-  ],
-  credentials: true
-}));
-dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-	app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
+    app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
 });
